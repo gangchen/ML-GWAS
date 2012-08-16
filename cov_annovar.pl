77dac888412f8a3511ea0be751016966ae60530c
Gang Chen <chengangcs@gmail.com>
2012-08-16 17:30:01 +0800

    reads based encoding

--- cov_annovar.pl
+++ cov_annovar.pl
@@ -23,22 +23,21 @@ my $map = {
 		"C" => "CC",
 		"G" => "GG",
 		"T" => "TT",
-                "W" => "AT","S" => "CG",
-                "K" => "TG","M" => "AC",
-                "Y" => "TC","R" => "AG",};
-
-# my $encoding = {"aa" => ["1", "0", "0"],
-# 				"ab" => ["0", "1", "0"],
-# 				"bb" => ["0", "0", "1"],
-# 				"0"  => ["0", "0", "0"],};
-my $encoding = {"aa" => ["1", "0"],
-				"ab" => ["0.5", "0.5"],
-				"bb" => ["0", "1"],
-				"0"  => ["0", "0"],};
+        "W" => "AT",
+        "S" => "CG",
+        "K" => "TG",
+        "M" => "AC",
+        "Y" => "TC",
+        "R" => "AG",};
 
 
-open(my $snpFile, "psoriasis_all.snp") or die $!;
-open(my $dataFile, ">4X_20_80_2bit_psoriasis.txt") or die $!;
+my $snpFilename = shift;
+my $dataFilename = shift;
+my $annovarFilename = shift;
+
+open(my $snpFile, $snpFilename) or die $!;
+open(my $dataFile, ">".$snpFilename) or die $!;
+open(my $annovarFile, ">".$annovarFilename) or die $!;
 
 while(my $line = <$snpFile>){
 	chomp $line;
@@ -48,84 +47,68 @@ while(my $line = <$snpFile>){
 	my $pos = $fields[1];
 	my $numCall = 0;
 	my %genotypes;
+	my @samples;
 	for my $field ( @fields[4 .. $#fields] ){
-		$field =~ /(.)(\d+).(\d+).(\d+)/;
-		my $deepth = $3 + $4;
-		my $genotype = $1;
-		my $quality = $2;
-		#print $deepth, "\t", $genotype, "\t", $quality, "\n";
-		if($deepth >= 4 and $quality >= 20){
-			my ($genotype1, $genotype2) = split "", $map->{$genotype};
-			$genotypes{$genotype1}++;
-			$genotypes{$genotype2}++;
-			$numCall++;
+		$field =~ /(.)(\d+)(.)(\d+)(.)(\d+)/;
+		my $g1 = $3;
+		my $g2 = $5;
+		my $d1 = $4;
+		my $d2 = $6;
+		my %tmp;
+		# determine the major allel by number of reads
+		my ($genotype1, $genotype2) = split "", $map->{$g1};
+		$genotypes{$genotype1} += $d1;
+		$genotypes{$genotype2} += $d1;
+		$tmp{$genotype1} += $d1;
+		$tmp{$genotype2} += $d1;
+		($genotype1, $genotype2) = split "", $map->{$g2};
+		$genotypes{$genotype1} += $d1;
+		$genotypes{$genotype2} += $d1;
+		$tmp{$genotype1} += $d2;
+		$tmp{$genotype2} += $d2;
+		push @samples, \%tmp;
+	}
+
+	my $majorSNP = "X";
+	my $minorSNP = "X";
+	
+	for my $snp (keys %genotypes){
+		if(!exists($genotypes{$majorSNP}) or 
+			$genotypes{$majorSNP} < $genotypes{$snp}){
+			$majorSNP = $snp;
 		}
 	}
 
-	if(scalar keys %genotypes >= 2 and ($numCall/1500) >= 0.8 ){
-		my $majorSNP = "XX";
-		my $minorSNP = "XX";
-		
-		for my $snp (keys %genotypes){
-			#print $snp, ":", $genotypes{$snp}, "\t";
-			if(!defined($genotypes{$majorSNP}) or 
-				$genotypes{$majorSNP} < $genotypes{$snp}){
-				$majorSNP = $snp;
-			}
+	for my $snp (keys %genotypes){
+		next if($snp eq $majorSNP);
+		if(!exists($genotypes{$minorSNP}) or 
+			$genotypes{$minorSNP} < $genotypes{$snp}){
+			$minorSNP = $snp;
 		}
-
-		for my $snp (keys %genotypes){
-			next if($snp eq $majorSNP);
-			if(!defined($genotypes{$minorSNP}) or 
-				$genotypes{$minorSNP} < $genotypes{$snp}){
-				$minorSNP = $snp;
-			}
-		}
-		 # print $chr, "\t", $pos, "\t", $pos, "\t", $majorSNP, "\t", $minorSNP, "\tcomments: ";
-		 # if($genotypes{$minorSNP}/($genotypes{$minorSNP}+$genotypes{$majorSNP}) < 0.01 ){
-			# print "rare SNP\n";
-		 # }else{
-		 # 	print "common SNP\n";
-		 # }
-
-		my @features;
-		my $feature;
-		for my $field ( @fields[4 .. $#fields] ){
-			$field =~ /(.)(\d+).(\d+).(\d+)/;
-			my $deepth = $3 + $4;
-			my $genotype = $1;
-			my $quality = $2;
-			if($deepth >= 4 and $quality >= 20){
-				my ($genotype1, $genotype2) = split "", $map->{$genotype};
-				if($genotype1 eq $genotype2){
-					if($genotype1 eq $majorSNP){
-						push @features, $_ for(@{$encoding->{"aa"}});
-					}elsif($genotype1 eq $minorSNP){
-						push @features, $_ for(@{$encoding->{"bb"}});
-					}else{
-						push @features, $_ for(@{$encoding->{"0"}});
-					}
-				}elsif($genotype1 ne $genotype2 and
-						(($genotype1 eq $majorSNP and $genotype2 eq $minorSNP) or
-						($genotype2 eq $majorSNP and $genotype1 eq $minorSNP))){
-					push @features, $_ for(@{$encoding->{"ab"}});
-				}else{
-					push @features, $_ for(@{$encoding->{"0"}});
-				}
-			}else{
-				push @features, $_ for(@{$encoding->{"0"}});
-			}
-		}
-		my @nums1 = map {2 * ($_-1)} 1..((scalar @features)/2);
-		my @nums2 = map {2 * ($_-1) + 1} 1..((scalar @features)/2);
-		#my @nums3 = map {3 * ($_-1) + 2} 1..((scalar @features)/3);
-		print $dataFile $chr, "-", $pos, "-1\t", join("\t", @features[@nums1]), "\n";
-		print $dataFile $chr, "-", $pos, "-2\t", join("\t", @features[@nums2]), "\n";
-		#print $dataFile $chr, "-", $pos, "-3\t", join("\t", @features[@nums3]), "\n";
 	}
+	print $annovarFile $chr, "\t", $pos, "\t", $pos, "\t", $majorSNP, "\t", $minorSNP, "\tcomments\n";
 
+	print $dataFile $chr, "-", $pos, "-1";
+	for(@samples){
+		if(exists($_->{$majorSNP})){
+			print $dataFile "\t",$_->{$majorSNP};
+		}else{
+			print $dataFile "\t","0";
+		}
+	}
+	print $dataFile "\n";
+	print $dataFile $chr, "-", $pos, "-2";
+	for(@samples){
+		if(exists($_->{$minorSNP})){
+			print $dataFile "\t",$_->{$minorSNP};
+		}else{
+			print $dataFile "\t","0";
+		}
+	}
+	print $dataFile "\n";
 
 }
+close $annovarFile;
 close $dataFile;
 close $snpFile;
 
